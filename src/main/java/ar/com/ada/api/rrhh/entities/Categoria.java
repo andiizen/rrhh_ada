1ec369b763c990b1ac013c1bc77ed67e1385cc01
andiizen <andiizenklusen@hotmail.com>
2020-06-18 09:30:11 -0300

    Se crea proyecto SpringBoot. Se crean las clases categoria y empleado. Tambien las clases repository, services y controllers.

---
+++ src/main/java/ar/com/ada/api/rrhh/entities/Categoria.java
@@ -0,0 +1,10 @@
+package ar.com.ada.api.rrhh.entities;
+import java.math.BigDecimal;
+
+public class Categoria {
+
+    private int categoriaId;
+    private String nombre;
+    private BigDecimal sueldoBase;
+    
+}
