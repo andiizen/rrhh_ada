b1aa6af1d07bb7db84bf6541d3c528c112ba9ba6
andiizen <andiizenklusen@hotmail.com>
2020-06-18 11:49:35 -0300

    Se crean los controllers con los metodos respectivos. Se agregan datos de conexion a base de datos.

--- src/main/java/ar/com/ada/api/rrhh/services/CategoriaService.java
+++ src/main/java/ar/com/ada/api/rrhh/services/CategoriaService.java
@@ -2,11 +2,15 @@ package ar.com.ada.api.rrhh.services;
 
 import java.util.*;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import ar.com.ada.api.rrhh.entities.*;
 import ar.com.ada.api.rrhh.repos.CategoriaRepository;
 
+@Service
 public class CategoriaService {
-
+    @Autowired
     CategoriaRepository repository;
 
     public void crearCategoria(Categoria categoria) {
