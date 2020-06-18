3e7c9a9e625e4474242f63ecf46d7faf9160e760
andiizen <andiizenklusen@hotmail.com>
2020-06-18 10:37:05 -0300

    Se crean metodos en CategoriaService para crear , guardar y para devolver la lista completa de la misma.

--- src/main/java/ar/com/ada/api/rrhh/repos/CategoriaRepository.java
+++ src/main/java/ar/com/ada/api/rrhh/repos/CategoriaRepository.java
@@ -1,6 +1,7 @@
 package ar.com.ada.api.rrhh.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import ar.com.ada.api.rrhh.entities.Categoria;
 
--- src/main/java/ar/com/ada/api/rrhh/services/CategoriaService.java
+++ src/main/java/ar/com/ada/api/rrhh/services/CategoriaService.java
@@ -1,6 +1,22 @@
 package ar.com.ada.api.rrhh.services;
 
+import java.util.*;
+
+import ar.com.ada.api.rrhh.entities.*;
+import ar.com.ada.api.rrhh.repos.CategoriaRepository;
+
 public class CategoriaService {
-    
-    
+
+    CategoriaRepository repository;
+
+    public void crearCategoria(Categoria categoria) {
+        repository.save(categoria);
+
+    }
+
+    public List<Categoria> listarCategorias() {
+
+        return repository.findAll();
+
+    }
 }
